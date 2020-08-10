dfe380085b1b962e148e47dea6de3e9bb075f228
Dave <dave.cadelina@outlook.com>
2020-08-11 00:10:27 +1000

    added alert if date entered is not future date and time

--- app.js
+++ app.js
@@ -14,6 +14,11 @@ const getTimeDifference = (start, end) => {
     let hours = Math.floor(minutes / 60);
     let days = Math.floor(hours / 24);
 
+    if(milliseconds < 0) {
+        alert("Please enter a future date and time")
+        return [0,0,0,0]
+    }
+
     hours = hours - (days * 24);
     minutes = minutes - (days * 24 * 60) - (hours * 60);
     seconds = seconds - (days * 24 * 60 * 60) - (hours * 60 * 60) - (minutes * 60);
