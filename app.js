f06be50b9d2900b7e76605d4faea4fdc1a955d9b
Dave <dave.cadelina@outlook.com>
2020-08-11 18:34:30 +1000

    added styles and accounted for invalid inputs

--- app.js
+++ app.js
@@ -14,16 +14,16 @@ const getTimeDifference = (start, end) => {
     let hours = Math.floor(minutes / 60);
     let days = Math.floor(hours / 24);
 
-    if(milliseconds < 0) {
+    if (milliseconds > 0 && end > 0) {
+        hours = hours - (days * 24);
+        minutes = minutes - (days * 24 * 60) - (hours * 60);
+        seconds = seconds - (days * 24 * 60 * 60) - (hours * 60 * 60) - (minutes * 60);
+        return [days, hours, minutes, seconds]
+    } else {
         alert("Please enter a future date and time")
-        return [0,0,0,0]
+        return [0, 0, 0, 0]
     }
 
-    hours = hours - (days * 24);
-    minutes = minutes - (days * 24 * 60) - (hours * 60);
-    seconds = seconds - (days * 24 * 60 * 60) - (hours * 60 * 60) - (minutes * 60);
-
-    return [days, hours, minutes, seconds]
 }
 
 function checkZero(number) {
